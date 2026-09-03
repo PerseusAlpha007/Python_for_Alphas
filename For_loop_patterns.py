83cba4b168f68ae29310707c070e41df79fc7229
PerseusAlpha007 <mdadeem7002@gmail.com>
2026-09-03 18:55:12 +0530

    All for loop pattern code are here.

--- For_loop_patterns.py
+++ For_loop_patterns.py
@@ -1,19 +1,19 @@
-# for i in range(1,int(input("Enter No. of rows :"))+1):
-#     for j in range(i):
-#         print(i,end = " ")
-#     print()
+for i in range(1,int(input("Enter No. of rows :"))+1):
+    for j in range(i):
+        print(i,end = " ")
+    print()
 
-# c = input("Enter a character :")
-# for i in range(1,int(input("Enter No. of rows :"))+1):
-#     for j in range(i):
-#         print(c,end = " ")
-#     print()
+c = input("Enter a character :")
+for i in range(1,int(input("Enter No. of rows :"))+1):
+    for j in range(i):
+        print(c,end = " ")
+    print()
 
-# k = "A"
-# for i in range(1,int(input("Enter No. of rows :"))+1):
-#     for j in range(i):
-#         print(k,end = " ")
-#         k = ord(k)
-#         k += 1
-#         k = chr(k)        
-#     print() 
+k = "A"
+for i in range(1,int(input("Enter No. of rows :"))+1):
+    for j in range(i):
+        print(k,end = " ")
+        k = ord(k)
+        k += 1
+        k = chr(k)        
+    print() 
