a1c8bc3b46949cb73128cce34a444674ef427f64
Mohammad Adeem_007 <mdadeem7002@gmail.com>
2026-09-15 22:17:46 +0530

    Add files via upload

--- Types of arguments.py
+++ Types of arguments.py
@@ -3,14 +3,14 @@ Positional Argument
 in this kind of argument we can not change the positions of paramerters in the
 argument parenthesis
 '''
-# def result(seat_no,name,marks):
-#     print("Seat_no. = ",seat_no)
-#     print("Name = ",name)
-#     print("Percentage = ",sum(marks)/len(marks))
+def result(seat_no,name,marks):
+    print("Seat_no. = ",seat_no)
+    print("Name = ",name)
+    print("Percentage = ",sum(marks)/len(marks))
 
 
-# result("Ashmir",54218721,[54,65,98,78,58])
-# print(result)
+result("Ashmir",54218721,[54,65,98,78,58])
+print(result)
 
 '''
 Keyworded Argument
@@ -19,14 +19,14 @@ we have to specify in function cal both the variable and the corrosponding
 value
 '''
 
-# def result(seat_no,name,marks):
-#     print("Seat_no. = ",seat_no)
-#     print("Name = ",name)
-#     print("Percentage = ",sum(marks)/len(marks))
+def result(seat_no,name,marks):
+    print("Seat_no. = ",seat_no)
+    print("Name = ",name)
+    print("Percentage = ",sum(marks)/len(marks))
 
 
-# result(name = "Ashmir",seat_no = 54218721,marks = [54,65,98,78,58])
-# print(result)
+result(name = "Ashmir",seat_no = 54218721,marks = [54,65,98,78,58])
+print(result)
 '''
 Arbitariry Argument
 In this kind of argument we can pass multiple arguments in one variable 
@@ -34,13 +34,13 @@ in the for of tuple using "*" before the variable in the function definition
 NOTE = The Arbitariry argument must always be at the end of parameter parenthesis
 '''
 
-# def display_student_data(name,*marks):
-#     print("Name = ",name )
-#     print("Marks = ",*marks)
+def display_student_data(name,*marks):
+    print("Name = ",name )
+    print("Marks = ",*marks)
 
 
-# display_student_data("Sandy",45,32,89,97,45,)
-# display_student_data("Tina",78,65,9,68,75)
+display_student_data("Sandy",45,32,89,97,45,)
+display_student_data("Tina",78,65,9,68,75)
 
 '''
 Keyworded Arbitariry Argument
